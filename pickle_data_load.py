a9034dd7d447bd2199f61205098d7b0054239846
BubbleShadow <yifei.han@uconn.edu>
2020-05-01 16:18:01 -0400

    first commit

---
+++ pickle_data_load.py
@@ -0,0 +1,17 @@
+import pickle
+import argparse
+
+parser = argparse.ArgumentParser(description='Configuration for loading pickle files')
+parser.add_argument('--path', type=str,
+                    help='File path to pickle files',
+                    default='/Users/jacobhan/Desktop/Brandeis Academic/Spring 2020/COSI 132/proj')
+args = parser.parse_args()
+
+def print_this_pickle(pickle_number):
+    file_name = args.path + str(pickle)
+    with open(args.path + "/1.pkl", "rb") as f:
+        articles = pickle.load(f)
+        print(articles)
+
+if __name__ == "__main__":
+    print_this_pickle(1)

a1ca865d8754c99bb2567f60c53a60c74eb7568c
BubbleShadow <yifei.han@uconn.edu>
2020-05-07 17:05:48 -0400

    Updated Pandas Load Data
    completed simple preprocss within pandas

---
+++ LDA.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+from pprint import pprint
+
+# Gensim
+import gensim
+import gensim.corpora as corpora
+from gensim.utils import simple_preprocess
+from gensim.models import CoherenceModel
+
+# spacy for lemmatization
+import spacy
+
+# Plotting tools
+import pyLDAvis
+import pyLDAvis.gensim
+import matplotlib.pyplot as plt
+
+# Build LDA model
+lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
+                                           id2word=id2word,
+                                           num_topics=20,
+                                           random_state=100,
+                                           update_every=1,
+                                           chunksize=100,
+                                           passes=10,
+                                           alpha='auto',
+                                           per_word_topics=True)
+
+print ("this is it!")
--- pickle_data_load.py
+++ pickle_data_load.py
@@ -1,6 +1,14 @@
 import pickle
 import argparse
 
+import gensim
+import gensim.corpora as corpora
+from gensim.utils import simple_preprocess
+from gensim.models import CoherenceModel
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+
 parser = argparse.ArgumentParser(description='Configuration for loading pickle files')
 parser.add_argument('--path', type=str,
                     help='File path to pickle files',
@@ -10,8 +18,48 @@ args = parser.parse_args()
 def print_this_pickle(pickle_number):
     file_name = args.path + "/" + str(pickle_number) + ".pkl"
     with open(file_name, "rb") as f:
-        articles = pickle.load(f)
-        print(articles)
+        articles = pickle.load(f) # articles: a dict with paper ID as keys
+        for b in articles:
+            print (b, articles[b])
+
+def load_into_pandas(pickle_number):
+    file_name = args.path + "/" + str(pickle_number) + ".pkl"
+    with open(file_name, "rb") as f:
+        articles = pickle.load(f) # articles: a dict with paper ID as keys
+        df = pd.DataFrame.from_dict(articles)
+        df = df.T
+        print(df)
+        return df
+
+def append_to_pandas_df(pickle_number, dataframe):
+    file_name = args.path + "/" + str(pickle_number) + ".pkl"
+    with open(file_name, "rb") as f:
+        articles = pickle.load(f) # articles: a dict with paper ID as keys
+        df = pd.DataFrame.from_dict(articles)
+        df = df.T
+        dataframe = dataframe.append(df)
+        print(dataframe)
+        return dataframe
+
+
+def simple_preprocess(dataframe, column):
+    """
+    input dataframe and the column of the dataframe to preprocess
+    entries within that column must be string
+
+    :return: dataframe with column specified preprocessed
+    """
+    for each_entry in dataframe[column]:
+        idx = dataframe.index[dataframe[column] == each_entry]
+        processed_entry = gensim.utils.simple_preprocess(str(each_entry), deacc=True)
+        dataframe.at[idx.to_list()[0], column] = processed_entry
+    return dataframe
 
 if __name__ == "__main__":
-    print_this_pickle(1)
+    #print_this_pickle(1)
+    df_1 = load_into_pandas(1)
+    #df_2  = append_to_pandas_df(2, df_1)
+    pre = simple_preprocess(df_1, "title")
+    pre = simple_preprocess(pre, "abstract")
+    pre = simple_preprocess(pre, "text")
+    print (pre)
